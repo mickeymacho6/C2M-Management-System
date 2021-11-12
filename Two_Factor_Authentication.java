1cebc6828f651113c50779dafd5b5e1b78f90f3a
vyas0320 <vyas0320@gmail.com>
2021-11-12 13:26:35 -0800

    Create Two_Factor_Authentication.java

---
+++ Two_Factor_Authentication.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Two_Factor_Authentication {
+
+    private static JButton sendCodeByEmail;
+    private static JButton sendCodeByText;
+    private static JButton verifyCode;
+
+    public static void main(String [] args) {
+
+
+
+
+    }
+}
