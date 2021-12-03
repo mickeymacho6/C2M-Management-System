d24d352acd0f3043aaa42cc0165848dd24a56179
TrisdonZepeda <90161020+TrisdonZepeda@users.noreply.github.com>
2021-12-03 13:55:56 -0800

    refined UI with buttons

--- C2M.java
+++ C2M.java
@@ -1,29 +1,108 @@
 package C2MProto;
 
 import com.codename1.components.ImageViewer;
+import com.codename1.components.InteractionDialog;
 import com.codename1.components.ShareButton;
-import com.codename1.ui.Button;
-import com.codename1.ui.Container;
-import com.codename1.ui.Form;
-import com.codename1.ui.Image;
+import com.codename1.ui.*;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.geom.Dimension;
 import com.codename1.ui.layouts.BorderLayout;
 import javax.swing.*;
 import java.io.IOException;
+
+import com.codename1.ui.layouts.FlowLayout;
+import com.codename1.ui.layouts.GridLayout;
+import com.codename1.ui.layouts.LayeredLayout;
+import com.codename1.ui.scene.TextPainter;
+import com.codename1.ui.plaf.Style;
 
 
 public class C2M extends Form {
 
     private UiDrawer UI;
+    Image buttonImage;
+
     public C2M() {
+
+        try{
+            buttonImage = Image.createImage("/optionButton.png");
+
+        }catch (IOException e) {e.printStackTrace();}
+
 
 
         UI = new UiDrawer();
+        Button inventoryButton = new Button(buttonImage);
+        Button packageButton = new Button(buttonImage);
+        Button transactionButton = new Button(buttonImage);
+
+        Container buttons = new Container();
+        buttons.add(inventoryButton);
+        buttons.add(packageButton);
+        buttons.add(transactionButton);
+
+
+        //Placeholder messages for clicking option tabs. When homepage is linked to other pages this would be changed to open the selected page.
+        ActionListener selectInventory = new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                InteractionDialog promp = new InteractionDialog();
+                Container option = new Container(new GridLayout(1, 1));
+                promp .setLayout(new BorderLayout());
+                promp .add(BorderLayout.CENTER, new Label("Would open another page"));
+                Button ok = new Button("ok");
+                ok.addActionListener((ee)-> promp .dispose());
+                option.add(ok);
+                promp.addComponent(BorderLayout.SOUTH, option);
+                promp.show(100, 100, 100, 100);
+            }
+        };
+
+        ActionListener selectPackage = new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                InteractionDialog promp = new InteractionDialog();
+                Container option = new Container(new GridLayout(1, 1));
+                promp .setLayout(new BorderLayout());
+                promp .add(BorderLayout.CENTER, new Label("Would open another page"));
+                Button ok = new Button("ok");
+                ok.addActionListener((ee)-> promp .dispose());
+                option.add(ok);
+                promp.addComponent(BorderLayout.SOUTH, option);
+                promp.show(100, 100, 100, 100);
+            }
+        };
+
+        ActionListener selectTransaction = new ActionListener()
+        {
+
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                InteractionDialog promp = new InteractionDialog();
+                Container option = new Container(new GridLayout(1, 1));
+                promp .setLayout(new BorderLayout());
+                promp .add(BorderLayout.CENTER, new Label("Would open another page"));
+                Button ok = new Button("ok");
+                ok.addActionListener((ee)-> promp .dispose());
+                option.add(ok);
+                promp.addComponent(BorderLayout.SOUTH, option);
+                promp.show(100, 100, 100, 100);
+            }
+        };
+
+        inventoryButton.addActionListener(selectInventory);
+        packageButton.addActionListener(selectPackage);
+        transactionButton.addActionListener(selectTransaction);
+
         Form display = new Form("C2M UI Prototype", new BorderLayout());
-        display.add(BorderLayout.CENTER, UI);
+        display.add(BorderLayout.CENTER,LayeredLayout.encloseIn(UI, FlowLayout.encloseCenterBottom(buttons)));
         display.show();
-
-
-
+        
     }
 
 
--- UiDrawer.java
+++ UiDrawer.java
@@ -1,30 +1,40 @@
 package C2MProto;
 
 import com.codename1.charts.util.ColorUtil;
+import com.codename1.ui.Button;
 import com.codename1.ui.Image;
 import com.codename1.ui.Graphics;
 import java.io.IOException;
 
 import com.codename1.ui.Component;
+import com.codename1.ui.geom.Rectangle;
+
 
 public class UiDrawer extends Component {
+
     private int gold = ColorUtil.rgb(182, 149, 11);
     Image feature1, feature2, feature3, logo, banner, bannerButtons;
 
+    //Placeholder strings for information banner. When homepage is linked to other pages this will be replace by
+    // String = pageName.getInformationX();
+    String time = "Philippines     Pacific      Mountain      Central        Eastern",
+            time2="  2:12PM        10:12PM     11:12PM      12:12AM     1:12AM", currency = "1 USD = 50.40 PHP", lastTransaction = "Last Transaction: Order #123019    Shipping Status: Shipped    ", lastTransaction2 =
+            "Tracking Number: PO569037832012293     Estimated Delivery: Feb 14th, 2022";
+    String date = "Sunday November 28th, 2021";
     public UiDrawer(){
         try{
             feature1 = Image.createImage("/inventory.png");
             feature2 = Image.createImage("/package.png");
             feature3 = Image.createImage("/translog.png");
             logo = Image.createImage("/logo.png");
-            banner = Image.createImage("/banner.png");
+            banner = Image.createImage("/blankbanner.png");
             bannerButtons = Image.createImage("/bannerbuttons.png");
         }catch (IOException e) {e.printStackTrace();}
 
 
 
     }
-//Current version of paint creates the images scaled and place for 720p browser windows.
+//Current version of paint creates the images scaled and placed for 720p browser windows.
 //Next step will have paint check for the resolution of the window and scale/place the UI elements appropriately
     @Override
     public void paint(Graphics g) {
@@ -36,7 +46,19 @@ public class UiDrawer extends Component {
         g.drawImage(feature3, 810, 570);
         g.drawImage(bannerButtons, 0 , 70);
         g.drawImage(banner, 0, 0, 1280, 75);
-        g.drawImage(logo, 1090, 80);
+        g.drawImage(logo, 1095, 80);
+        g.drawString(time, 60, 10);
+        g.drawString(time2, 60, 30);
+        g.drawString(currency, 595, 30);
+        g.drawString(lastTransaction, 820, 10);
+        g.drawString(lastTransaction2, 820, 45);
+        g.setColor(ColorUtil.LTGRAY);
+        g.drawString(date, 1095, 260);
+
+
 
     }
+
+
+
 }
