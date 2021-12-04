b6c5c837c80e935b023aa3e8cab5a6ecb15c6946
mightymyles00 <43979996+mightymyles00@users.noreply.github.com>
2021-12-03 16:21:55 -0800

    Add files via upload

---
+++ FirstWBGui.java
@@ -0,0 +1,170 @@
+package windowBuilder.views;
+
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.border.CompoundBorder;
+import javax.swing.JRadioButton;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.DropMode;
+
+public class FirstWBGui extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FirstWBGui frame = new FirstWBGui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FirstWBGui() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 600);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.ORANGE);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		Object[] headerNames = {"Order ID", "Date Ordered", "Date Delivered", "Product Name", "Seller", "Price"};
+		
+		table = new JTable();
+		table.setFillsViewportHeight(true);
+		table.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		table.setModel(new DefaultTableModel(
+			new String[][] {
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"Order#", "Date Ordered", "Date Delivered", "Product Name", "Seller", "Price"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, String.class, String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(1).setPreferredWidth(86);
+		
+		JButton btnPrevious = new JButton("Previous");
+		btnPrevious.setForeground(Color.ORANGE);
+		btnPrevious.setBackground(Color.BLACK);
+		btnPrevious.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JButton btnNext = new JButton("Next");
+		btnNext.setForeground(Color.ORANGE);
+		btnNext.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnNext.setBackground(Color.BLACK);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Search");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		
+		JPanel panel = new JPanel();
+		
+		
+		//panel.add(new JLabel (new ImageIcon("/Card2Cart.png")));
+		panel.setBackground(Color.BLACK);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.BLACK);
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(42)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField, GroupLayout.PREFERRED_SIZE, 233, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(532, Short.MAX_VALUE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 320, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED, 433, Short.MAX_VALUE)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(595, Short.MAX_VALUE)
+					.addComponent(btnPrevious, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+					.addGap(55)
+					.addComponent(btnNext, GroupLayout.PREFERRED_SIZE, 108, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap())
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(65)
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 728, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(83, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 123, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
+							.addGap(27)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE))))
+					.addGap(24)
+					.addComponent(table, GroupLayout.PREFERRED_SIZE, 306, GroupLayout.PREFERRED_SIZE)
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNext, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnPrevious, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE))
+					.addGap(22))
+		);
+		
+		JLabel lblNewLabel_1 = new JLabel("Transaction History");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_1.setForeground(Color.ORANGE);
+		panel_1.add(lblNewLabel_1);
+		
+		JLabel image = new JLabel("");
+		image.setIcon(new ImageIcon("C:\\Users\\Owner\\eclipse-workspace\\TransactionHistory\\Card2Cart.png"));
+		image.setBounds(10, 53, 166, 256);
+		
+		panel.add(image);
+		
+		contentPane.setLayout(gl_contentPane);
+		
+		
+	}
+}
