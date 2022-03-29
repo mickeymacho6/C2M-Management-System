b7876bc0879b7e2dc1209f956cd4bccb7dd247c4
Reza Salmanian <93802122+reza1200@users.noreply.github.com>
2022-03-29 15:00:23 -0700

    C2M -112, 120
     The new version of the login page and registration form

---
+++ src/user.java
@@ -0,0 +1,12 @@
+public class user {
+    public String name;
+    public String email;
+    public String confirmEmail;
+    public String username;
+    public String password;
+    public String confirm_password;
+
+
+
+
+}
