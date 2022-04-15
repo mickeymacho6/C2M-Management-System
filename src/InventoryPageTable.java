b82f0848d2d7fdb0833763d76ffec857c01c1fb1
Ethan Wyant <wyantethan@gmail.com>
2022-04-15 12:31:47 -0700

    Tidied up inventory page, refactored with new layout. Added comments to show where current SQL code is being placed for database read / write

--- src/InventoryPageTable.java
+++ src/InventoryPageTable.java
@@ -2,10 +2,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class InventoryPageTable {
     private static JTextField card_name_field;
@@ -20,6 +17,7 @@ public class InventoryPageTable {
     private JTable search_class_table;
 
     public static void main(String[] args) {
+
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ JTable Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         JTable search_class_table = new JTable();
@@ -29,13 +27,16 @@ public class InventoryPageTable {
         JFrame search_class_frame = new JFrame("Search class");
         search_class_frame.getContentPane().setBackground(new Color(218, 165, 32));
         search_class_frame.getContentPane().setForeground(Color.WHITE);
-        search_class_frame.setBounds(100, 100, 962, 813);
+        search_class_frame.setBounds(100, 100, 1000, 800);
         search_class_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         search_class_frame.getContentPane().setLayout(null);
         search_class_frame.setLocationRelativeTo(null);
 
         model.setColumnIdentifiers(columns);
         search_class_table.setModel(model);
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Table Setup ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         search_class_table.setBackground(Color.white);
         search_class_table.setForeground(Color.black);
@@ -44,37 +45,38 @@ public class InventoryPageTable {
         search_class_table.setRowHeight(30);
         search_class_table.setAutoCreateRowSorter(true);
 
+
         JScrollPane pane = new JScrollPane(search_class_table);
         pane.setForeground(Color.RED);
         pane.setBackground(Color.WHITE);
-        pane.setBounds(133, 83, 667, 298);
+        pane.setBounds(10, 80, 600, 600);
         search_class_frame.getContentPane().add(pane);
 
         Object[] row = new Object[6];
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Page Title ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        JLabel lblInventoryPage = new JLabel("INVENTORY PAGE");
+        JLabel lblInventoryPage = new JLabel("C2M Inventory Page", SwingConstants.CENTER);
         lblInventoryPage.setOpaque(true);
         lblInventoryPage.setForeground(new Color(218, 165, 32));
         lblInventoryPage.setFont(new Font("Dialog", Font.PLAIN, 20));
         lblInventoryPage.setBackground(Color.BLACK);
-        lblInventoryPage.setBounds(0, 0, 289, 45);
+        lblInventoryPage.setBounds(0, 0, 230, 45);
         search_class_frame.getContentPane().add(lblInventoryPage);
 
         JLabel lblNewLabel_1 = new JLabel("");
-        lblNewLabel_1.setBounds(846, 0, 100, 100);
+        lblNewLabel_1.setBounds(850, 0, 100, 100);
         search_class_frame.getContentPane().add(lblNewLabel_1);
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Add Transactions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        JLabel lblAddItem = new JLabel("ADD NEW ITEM");
+        JLabel lblAddItem = new JLabel("Add New Card", SwingConstants.CENTER);
         lblAddItem.setOpaque(true);
         lblAddItem.setForeground(new Color(218, 165, 32));
         lblAddItem.setFont(new Font("Dialog", Font.PLAIN, 20));
         lblAddItem.setBackground(Color.BLACK);
-        lblAddItem.setBounds(143, 392, 191, 45);
+        lblAddItem.setBounds(725, 80, 150, 45);
         search_class_frame.getContentPane().add(lblAddItem);
 
 
@@ -91,9 +93,11 @@ public class InventoryPageTable {
                 row[3] = card_number_field.getText();
 
                 model.addRow(row);
+
+                // Insert SQL code here to write to database!
             }
         });
-        add_item_button.setBounds(62, 740, 136, 23);
+        add_item_button.setBounds(650, 315, 136, 23);
         search_class_frame.getContentPane().add(add_item_button);
 
 
@@ -105,60 +109,63 @@ public class InventoryPageTable {
                 int i = search_class_table.getSelectedRow();
                 if (i >= 0) {
                     model.removeRow(i);
+
+                    // Insert SQL code here to delete from database!
+
                 }
                 //
                 else JOptionPane.showMessageDialog(search_class_frame, "Delete error");
             }
         });
-        delete_button.setBounds(259, 740, 131, 23);
+        delete_button.setBounds(800, 315, 131, 23);
         search_class_frame.getContentPane().add(delete_button);
 
 
         // Seller field
         JLabel enter_card_name = new JLabel("Card Name");
-        enter_card_name.setBounds(203, 448, 89, 14);
+        enter_card_name.setBounds(675, 150, 80, 15);
         search_class_frame.getContentPane().add(enter_card_name);
 
         card_name_field = new JTextField();
-        card_name_field.setBounds(149, 473, 169, 20);
+        card_name_field.setBounds(625, 170, 160, 20);
         search_class_frame.getContentPane().add(card_name_field);
         card_name_field.setColumns(10);
 
-        // Card name field
+        // Card team field
         JLabel enter_card_label = new JLabel("Card Team");
-        enter_card_label.setBounds(203, 504, 115, 14);
+        enter_card_label.setBounds(675, 200, 80, 15);
         search_class_frame.getContentPane().add(enter_card_label);
 
         card_team_field = new JTextField();
-        card_team_field.setBounds(149, 529, 169, 20);
+        card_team_field.setBounds(625, 220, 160, 20);
         search_class_frame.getContentPane().add(card_team_field);
         card_team_field.setColumns(10);
 
         // Total Cost Field
         JLabel enter_cost_label = new JLabel("Card Cost");
-        enter_cost_label.setBounds(224, 560, 141, 14);
+        enter_cost_label.setBounds(675, 250, 140, 15);
         search_class_frame.getContentPane().add(enter_cost_label);
 
         card_cost_field = new JTextField();
-        card_cost_field.setBounds(196, 585, 169, 20);
+        card_cost_field.setBounds(625, 270, 160, 20);
         search_class_frame.getContentPane().add(card_cost_field);
         card_cost_field.setColumns(10);
 
 
-        // Tracking number field
+        // Card number field
         JLabel enter_card_number = new JLabel("Card Number");
-        enter_card_number.setBounds(224, 640, 141, 14);
+        enter_card_number.setBounds(840, 150, 140, 15);
         search_class_frame.getContentPane().add(enter_card_number);
 
         card_number_field = new JTextField();
         card_number_field.setColumns(10);
-        card_number_field.setBounds(196, 665, 169, 20);
+        card_number_field.setBounds(800, 170, 160, 20);
         search_class_frame.getContentPane().add(card_number_field);
 
 
-        // Shipping status combo box
+        // Card condition combo box
         JLabel cardCondition = new JLabel("Card Condition");
-        cardCondition.setBounds(80, 641, 200, 14);
+        cardCondition.setBounds(840, 2000, 200, 15);
         search_class_frame.getContentPane().add(cardCondition);
 
         JComboBox cardConditionComboBox = new JComboBox();
@@ -169,13 +176,13 @@ public class InventoryPageTable {
             }
         });
         cardConditionComboBox.setModel(new DefaultComboBoxModel(new String[] {"Perfect", "Good", "Fair", "Poor"}));
-        cardConditionComboBox.setBounds(80, 664, 75, 22);
+        cardConditionComboBox.setBounds(800, 220, 160, 20);
         search_class_frame.getContentPane().add(cardConditionComboBox);
 
 
-        // Payment status combo box
+        // Stock status combo box
         JLabel inStockLabel = new JLabel("In Stock?");
-        inStockLabel.setBounds(80, 560, 200, 14);
+        inStockLabel.setBounds(840, 250, 200, 15);
         search_class_frame.getContentPane().add(inStockLabel);
 
         JComboBox inStockComboBox = new JComboBox();
@@ -188,25 +195,25 @@ public class InventoryPageTable {
         });
 
         inStockComboBox.setModel(new DefaultComboBoxModel(new String[] {"Yes", "No"}));
-        inStockComboBox.setBounds(80, 584, 50, 22);
+        inStockComboBox.setBounds(800, 270, 160, 20);
         search_class_frame.getContentPane().add(inStockComboBox);
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Search Transactions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         // UI Element
-        JLabel search_transaction_label = new JLabel("SEARCH INVENTORY");
+        JLabel search_transaction_label = new JLabel("Search Inventory", SwingConstants.CENTER);
         search_transaction_label.setOpaque(true);
         search_transaction_label.setForeground(new Color(218, 165, 32));
         search_transaction_label.setFont(new Font("Dialog", Font.PLAIN, 20));
         search_transaction_label.setBackground(Color.BLACK);
-        search_transaction_label.setBounds(579, 392, 232, 45);
+        search_transaction_label.setBounds(700, 380, 200, 45);
         search_class_frame.getContentPane().add(search_transaction_label);
 
 
         // Search by seller
         JLabel card_name_enter_field = new JLabel("Card name");
-        card_name_enter_field.setBounds(664, 448, 103, 14);
+        card_name_enter_field.setBounds(760, 450, 100, 15);
         search_class_frame.getContentPane().add(card_name_enter_field);
 
         search_card_name_field = new JTextField();
@@ -220,14 +227,14 @@ public class InventoryPageTable {
 
             }
         });
-        search_card_name_field.setBounds(613, 473, 169, 20);
+        search_card_name_field.setBounds(715, 470, 160, 20);
         search_class_frame.getContentPane().add(search_card_name_field);
         search_card_name_field.setColumns(10);
 
 
         // Search by card name
         JLabel team_name_label = new JLabel("Team name");
-        team_name_label.setBounds(664, 504, 136, 14);
+        team_name_label.setBounds(760, 500, 140, 15);
         search_class_frame.getContentPane().add(team_name_label);
 
         search_card_team_field = new JTextField();
@@ -241,20 +248,14 @@ public class InventoryPageTable {
                 table_row.setRowFilter(RowFilter.regexFilter(search_card_team_field.getText().trim()));
             }
         });
-        search_card_team_field.setBounds(613, 529, 169, 20);
+        search_card_team_field.setBounds(715, 529, 160, 20);
         search_class_frame.getContentPane().add(search_card_team_field);
         search_card_team_field.setColumns(10);
 
 
-        JLabel test_label = new JLabel("TEST");
-        test_label.setBounds(900, 560, 103, 14);
-        search_class_frame.getContentPane().add(test_label);
-
-
-
         // Search by card cost
         JLabel cost_label = new JLabel("Card cost");
-        cost_label.setBounds(664, 560, 103, 14);
+        cost_label.setBounds(760, 560, 100, 15);
         search_class_frame.getContentPane().add(cost_label);
 
         search_cost_field = new JTextField();
@@ -268,20 +269,20 @@ public class InventoryPageTable {
             }
         });
         search_cost_field.setText("");
-        search_cost_field.setBounds(613, 582, 169, 20);
+        search_cost_field.setBounds(715, 582, 160, 20);
         search_class_frame.getContentPane().add(search_cost_field);
         search_cost_field.setColumns(10);
 
 
         // Search by tracking number
         JLabel card_number_label = new JLabel("Card Number");
-        card_number_label.setBounds(664, 613, 103, 14);
+        card_number_label.setBounds(760, 610, 100, 15);
         search_class_frame.getContentPane().add(card_number_label);
 
         search_card_number_field = new JTextField();
         search_card_number_field.setText("");
         search_card_number_field.setColumns(10);
-        search_card_number_field.setBounds(613, 634, 169, 20);
+        search_card_number_field.setBounds(715, 634, 160, 20);
         search_class_frame.getContentPane().add(search_card_number_field);
 
 
@@ -292,6 +293,5 @@ public class InventoryPageTable {
 
 
 
-    }//main
-
+    }
 }
